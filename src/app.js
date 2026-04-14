a2e051572e520f1dfa29e1d0dceb9cebf9336dd4
Your Name <your.email@example.com>
2026-04-14 16:45:39 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -32,8 +32,8 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // CREATE DEFAULT ADMIN
 const createAdmin = async () => {
