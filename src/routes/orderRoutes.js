cb693c0126fc749b2dd0bbd0e844bfeb1bc5edd9
Your Name <your.email@example.com>
2026-06-10 23:05:53 +0530

    updated

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Order from "../models/Order.js";
+import Order from "../models/order.js";
 import Notification from "../models/Notification.js";
 import nodemailer from "nodemailer";
 
