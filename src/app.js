762c2862179f634f3216f875adf7946c8d2c99f3
saminnam <innam8264@gmail.com>
2026-06-15 23:22:50 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -33,8 +33,8 @@ const app = express();
 // ✅ CORS (ensure headers are present even on OPTIONS / preflight)
 const corsOptions = {
   origin: [
-    'https://e-commerce-app-admin-pi.vercel.app',
-    'https://e-commerce-app-gray-one.vercel.app',
+    'https://admin.baqavibookcentre.com',
+    'https://www.baqavibookcentre.com',
     'http://localhost:5174',
     'http://localhost:5173',
   ],
