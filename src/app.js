7743127e9c428887a19a6fc23562b315e0dbb855
saminnam <innam8264@gmail.com>
2026-06-19 22:47:39 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import connectDB from "./config/db.js";
