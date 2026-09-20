a3fd603eb8847a2874eaaa9946e2dac73f26b560
developerInnam <developerinnam111@gmail.com>
2026-09-20 15:39:53 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -37,6 +37,7 @@ const corsOptions = {
     'https://admin.baqavibookcentre.com',
     'https://www.baqavibookcentre.com',
     'https://baqavibookcentre.com',
+    'https://list.baqavibookcentre.com',
     'http://localhost:5174',
     'http://localhost:5173',
   ],
